bb92fd999351b98818fa870f8e1cb07223e45fd5
Karl Morisset <karl.morisset@wildcodeschool>
2021-11-23 15:24:14 +0100

    Correction du manager dans Home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,16 +6,6 @@ use App\Model\CharacterManager;
 
 class HomeController extends AbstractController
 {
-    protected CharacterManager $manager;
-
-    public function __construct()
-    {
-        parent::__construct();
-
-        $this->manager = new CharacterManager();
-    }
-
-
     public function index()
     {
         return $this->twig->render('Home/index.html.twig');
